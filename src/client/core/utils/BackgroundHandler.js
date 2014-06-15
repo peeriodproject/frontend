08ae486279043f083017c83fafe751ecbd0fb51b
Jörn Röder <kontakt@joernroeder.de>
2014-06-15 20:24:36 +0200

    updated BackgroundHandler

--- src/client/core/utils/BackgroundHandler.js
+++ src/client/core/utils/BackgroundHandler.js
@@ -4,43 +4,97 @@
 'use strict';
 
 var React = require('react');
+var Chroma = require('chroma-js');
+
+window.Chroma = Chroma;
 
 var BackgroundHandler = React.createClass({
 
-	getInitialState: function() {
+	getDefaultProps: function () {
 		return {
-			secondsElapsed: 0,
-			color: '#fff'
+			minContrast: 4.5,
+			activeAlpha: 0.2,
+			hoverAlpha: 0.1,
+			transitionKeys: ['color', 'background-color', 'border-color'],
+			intervalInSeconds: 2
 		};
 	},
 
-	tick: function() {
-		this.setState({secondsElapsed: this.state.secondsElapsed + 1});
+	getInitialState: function () {
+		return {
+			secondsElapsed: Math.round(Math.random() * 255),
+			color: new Chroma('#fff'),
+			background: new Chroma('#000'),
+			invert: new Chroma('#000')
+		};
 	},
 
-	componentDidMount: function() {
-		this.interval = setInterval(this.tick, 1000);
+	tick: function () {
+		var secondsElapsed = this.state.secondsElapsed + 1;
+		var background = new Chroma('hsl(' + (this.state.secondsElapsed % 255) +  ', 100%, ' + (this.state.secondsElapsed % 100) + '%)');
+		var color = this.getColor();
+
+		if (Chroma.contrast(background, color) < this.props.minContrast) {
+			color = this.toggleColor();
+		}
+
+		this.setState({
+			secondsElapsed: secondsElapsed,
+			color: color,
+			invert: this.toggleColor(color),
+			background: background,
+		});
 	},
 
-	componentWillUnmount: function() {
+	toggleColor: function (color) {
+		var color = color || this.getColor();
+
+		return color.luminance() ? color.darken(100) : color.brighten(100);
+	},
+
+	componentDidMount: function () {
+		this.interval = setInterval(this.tick, this.props.intervalInSeconds * 1000);
+	},
+
+	componentWillUnmount: function () {
 		clearInterval(this.interval);
 	},
 
 	getBackgroundColor: function () {
-		return {
-			r: 0,
-			g: 0,
-			b: 0
-		};
+		return this.state.background;
 	},
 
-	createElementStyles: function (selector, styles) {
+	getColor: function () {
+		return this.state.color;
+	},
+
+	getInvertedColor: function () {
+		return this.state.invert;
+	},
+
+	getAlphaColor: function (alpha) {
+		return Chroma.scale([
+			this.getBackgroundColor(),
+			new Chroma(this.getColor())
+		])(alpha);
+	},
+
+	getTransitionForKey: function (key, transitionDuration) {
+		var duration = transitionDuration || this.props.intervalInSeconds;
+		return 'transition: ' + key + ' ' + duration + 's linear';
+	},
+
+	createElementStyles: function (selector, styles, transitionDuration) {
 		var keys = Object.keys(styles);
 		var style = [];
 		
 		for (var i = 0, l = keys.length; i < l; i++) {
 			var key = keys[i];
 			style.push(key + ': ' + styles[key]);
+
+			if (this.props.transitionKeys.indexOf(key) !== -1) {
+				style.push(this.getTransitionForKey(key, transitionDuration));
+			}
 		}
 
 		return selector + ' {\n\t' + style.join(';\n\t') + '\n}';
@@ -53,12 +107,42 @@ var BackgroundHandler = React.createClass({
 	render: function () {
 		//var wrapper = '#wrapper { background-color: ; }';
 		var styles = this.createStyles([
-			this.createElementStyles('#wrapper', {
-				'background-color': 'hsl(' + (this.state.secondsElapsed % 255) +  ', 60%, 20%)'
+			this.createElementStyles('#wrapper, .bg', {
+				'background-color': this.getBackgroundColor().hex()
+			}),
+
+			this.createElementStyles('.bg-hover', {
+				'background-color': this.getAlphaColor(this.props.hoverAlpha).hex()
+			}),
+
+			this.createElementStyles('.bg-active', {
+				'background-color': this.getAlphaColor(this.props.activeAlpha).hex()
+			}),
+
+			// color sheme
+			this.createElementStyles('.bg-color-light', {
+				'color': this.getAlphaColor(0.3).hex()
+			}),
+
+			this.createElementStyles('.bg-color-middle', {
+				'color': this.getAlphaColor(0.5).hex()
+			}),
+
+			this.createElementStyles('.bg-color-dark', {
+				'color': this.getAlphaColor(0.7).hex()
+			}),
+
+			this.createElementStyles('.bg-color-inverted', {
+				'color': this.getInvertedColor().hex()
+			}),
+
+			// border
+			this.createElementStyles('.bg-border', {
+				'border-color': this.getAlphaColor(0.15).hex()
 			}),
 
 			this.createElementStyles('body, a', {
-				color: this.state.color
+				'color': this.getColor().hex()
 			})
 		]);
 
