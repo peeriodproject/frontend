10db3ecb53d38f051fc548f66612435d7a99e552
Jörn Röder <kontakt@joernroeder.de>
2014-06-17 13:17:22 +0200

    added the ability to use multiple channels to ChannelMixin

--- src/client/core/socket/ChannelMixin.js
+++ src/client/core/socket/ChannelMixin.js
@@ -1,28 +1,61 @@
 var ChannelMixin = {
 
+	_channels: [],
+
+	_setupChannel: function (name) {
+		var self = this;
+		var channelName = name + 'Channel';
+		var onCallback = function () {
+			var args = arguments || [];
+
+			// add channelName as first argument
+			Array.prototype.unshift.call(args, name);
+
+			self.onChannelUpdate.apply(this, args);
+		};
+
+		this[channelName] = this.getChannel(name);
+
+		this[channelName].on('update', onCallback);
+		this[channelName].send('getInitialState', onCallback);
+	},
+
 	getChannel: function (channelName) {
 		return window.socket.channel(channelName);
 	},
 
+	firstCharToUpperCase: function (str) {
+		return str ? str[0].toUpperCase() + str.slice(1) : '';
+	},
+
 	getInitialState: function () {
-		if (!this.channelName) {
-			console.error('no channel name specified!');
+		var self = this;
+
+		if (!this.channelNames) {
+			console.error('no channel names specified!');
+			return;
 		}
 
-		if (!this.updateChannelState) {
-			console.error('no updateChannelState method implemented');
+		for (var i = 0, l = this.channelNames.length; i < l; i++) {
+			this._setupChannel(this.channelNames[i]);
+		};
+
+		return this.initialChannelsState;
+	},
+
+	onChannelUpdate: function () {
+		var args = arguments;
+		var channelUpdateName = 'update' + this.firstCharToUpperCase(args[0]) + 'ChannelState';
+
+		if (this[channelUpdateName]) {
+			// remove channelName from args
+			Array.prototype.shift.call(args);
+
+			this[channelUpdateName].apply(this, args);
 		}
-
-		if (!this.initialState) {
-			console.error('no initial state specified!');
+		else {
+			this.updateChannelState.apply(this, args);
 		}
-
-		this.channel = this.getChannel(this.channelName);
- 
-		this.channel.on('update', this.updateChannelState);
-		this.channel.send('getInitialState', this.updateChannelState);
-
-		return this.initialState;
 	}
 
 };
