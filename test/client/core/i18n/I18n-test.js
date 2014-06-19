bab1d821509292304e4153241ddcccfb1108e2f6
Jörn Röder <kontakt@joernroeder.de>
2014-06-19 14:06:08 +0200

    cleanup

--- test/client/core/i18n/I18n-test.js
+++ test/client/core/i18n/I18n-test.js
@@ -11,13 +11,13 @@ describe('CLIENT --> CORE --> I18N --> I18nMixin', function() {
 		};
 	});
 
-    it('returns the correct value for the given key', function() {
-        
-        var I18nMixin = require('../../../../src/client/core/i18n/I18nMixin');
-        var value = I18nMixin.i18n('foo');
+	it('returns the correct value for the given key', function() {
+		
+		var I18nMixin = require('../../../../src/client/core/i18n/I18nMixin');
+		var value = I18nMixin.i18n('foo');
 
-        expect(value).toEqual('value-foo');
-        expect(global.chrome.i18n.getMessage.mock.calls.length).toBe(1);
-        expect(global.chrome.i18n.getMessage.mock.calls[0][0]).toBe('foo');
-    });
+		expect(value).toEqual('value-foo');
+		expect(global.chrome.i18n.getMessage.mock.calls.length).toBe(1);
+		expect(global.chrome.i18n.getMessage.mock.calls[0][0]).toBe('foo');
+	});
 });
