e33f1fc40bde9b941a30d878f5842a411dd4b0a8
Jörn Röder <kontakt@joernroeder.de>
2014-07-02 18:41:47 +0200

    added FormFactory dummy

---
+++ src/client/core/form/formFactory.js
@@ -0,0 +1,22 @@
+/**
+ * @jsx React.DOM
+ */
+'use strict';
+
+var React = require('react');
+
+var newforms = require('newforms');
+
+var FormFactory = {
+
+	create: function (formData) {
+		var formData = {
+
+		};
+
+		//console.log(formData);
+	}
+
+}
+
+module.exports = FormFactory;
