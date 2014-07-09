a22b3813cfd5140dd2a8fef3711239086da89131
Jörn Röder <kontakt@joernroeder.de>
2014-07-09 12:02:12 +0200

    added extra layer and styles to SvgIcon

--- src/client/core/element/SvgIcon.js
+++ src/client/core/element/SvgIcon.js
@@ -7,9 +7,29 @@ var React = require('react');
 
 var SvgIcon = React.createClass({
 
+	getDefaultProps: function () {
+		return {
+			extraLayer: '',
+			extraStyles: ''
+		}
+	},
+
+	getSvg: function (layerName, styles) {
+		layerName = layerName ? ' class="' + layerName + '" ' : '';
+		styles = styles || '';
+
+		return '<svg' + layerName + ' style="' + styles + '"><use xlink:href="#icon-' + this.props.icon + '" /></svg>';
+	},
+
 	render: function () {
+		var svg = this.getSvg();
+
+		if (this.props.extraLayer) {
+			svg += this.getSvg(this.props.extraLayer, this.props.extraStyles);
+		}
+
 		return (
-			<span className={'icon icon-' + this.props.icon} dangerouslySetInnerHTML={{__html: '<svg><use xlink:href="#icon-' + this.props.icon + '" /></svg>' }} />
+			<span className={'icon icon-' + this.props.icon} dangerouslySetInnerHTML={{__html: svg }} />
 		);
 	}
 
