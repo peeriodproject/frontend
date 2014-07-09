b94f3fce7edbde953a5664c6d53da2ce5feec14c
Jörn Röder <kontakt@joernroeder.de>
2014-07-09 14:23:00 +0200

    added ConnectionFailedPage

---
+++ src/client/core/page/ConnectionFailedPage.js
@@ -0,0 +1,37 @@
+/**
+ * @jsx React.DOM
+ */
+'use strict';
+
+var React = require('react');
+
+var ConnectionFailedPage = React.createClass({
+
+	onReconnectClick: function (event) {
+		event.preventDefault();
+
+		window.location.reload();
+	},
+
+	render: function () {
+		return (
+			<main className='main'>
+				<section className='page connection-failed'>
+					<header>
+						<h1>Connection Failed</h1>
+					</header>
+					<p>
+						Vestibulum id ligula porta felis euismod semper. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.
+					</p>
+
+					<a href='#' className='btn' onClick={this.onReconnectClick}>
+						Reconnect
+					</a>
+				</section>
+			</main>
+		)
+	}
+
+});
+
+module.exports = ConnectionFailedPage;
--- src/client/main.js
+++ src/client/main.js
@@ -1,36 +1,89 @@
 var React = require('react');
-var BackgroundHandler = require('./core/utils/BackgroundHandler');
+//var BackgroundHandler = require('./core/utils/BackgroundHandler');
+var ConnectionFailedPage = require('./core/page/ConnectionFailedPage');
 var Router = require('./core/router/Router')(require('./config/routes'));
 
 var DialogHandler = require('./core/dialog/DialogHandler')({
 	'addFolderDialog': require('./core/dialog/AddFolderDialog')
 });
 
-// make socket available to components
-var socket = new Primus('http://localhost:7474/');
+window.App = {
+	_running: false,
+	_windowLoaded: false,
+	_connected: false,
+	_maxReconnectAttempts: 1,
 
-socket.on('reconnect', function () {
-	console.log('Reconnect attempt started');
-});
+	connect: function (attempts) {
+		attempts = attempts || 0;
 
-socket.on('reconnecting', function (opts) {
-	console.log('Reconnecting in %d ms', opts.timeout);
-	console.log('This is attempt %d out of %d', opts.attempt, opts.retries);
-});
+		console.log('Trying to connect to the backend...');
 
-socket.on('end', function () {
-	console.log('Connection closed');
-});
+		try {
+			attempts++;
 
-window.socket = socket;
+			// make socket available to components
+			var socket = new Primus('http://localhost:7474/');
 
-// enable the React developer tools!
-window.React = React;
+			socket.on('reconnect', function () {
+				console.log('Reconnect attempt started');
+			});
 
-if (typeof window !== 'undefined') {
-	window.onload = function() {
+			socket.on('reconnecting', function (opts) {
+				console.log('Reconnecting in %d ms', opts.timeout);
+				console.log('This is attempt %d out of %d', opts.attempt, opts.retries);
+			});
+
+			socket.on('end', function () {
+				App.connectionFailed();
+				//console.log('Connection closed');
+			});
+
+			window.socket = socket;
+
+			this._connected = true;
+
+			this.start();
+		}
+		catch (e) {
+			if (attempts <= this._maxReconnectAttempts) {
+				setTimeout(function () {
+					App.connect(attempts);
+				}, 1000);
+			}
+			else {
+				this.connectionFailed();
+			}
+		}
+	},
+
+	start: function () {
+		if (this._running || !this._windowLoaded || !this._connected) {
+			return;
+		}
+
 		React.renderComponent(DialogHandler(), document.getElementById('dialogHandler'));
 		//React.renderComponent(BackgroundHandler(), document.getElementById('backgroundHandler'));
 		React.renderComponent(Router(), document.getElementById('page'));
+
+		this._running = true;
+	},
+
+	connectionFailed: function () {
+		React.renderComponent(ConnectionFailedPage(), document.getElementById('page'));
+	}
+
+
+};
+
+App.connect();
+
+if (typeof window !== 'undefined') {
+	// enable the React developer tools!
+	window.React = React;
+
+	window.onload = function() {
+		App._windowLoaded = true;
+
+		App.start();
 	};
 }
