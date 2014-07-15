5605d481ab1d5fabfc06b44bb79f6c23a91056bc
Jörn Röder <kontakt@joernroeder.de>
2014-07-15 17:55:32 +0200

    download progress bar (on progress)

--- src/client/core/element/DownloadProgressBar.js
+++ src/client/core/element/DownloadProgressBar.js
@@ -5,12 +5,47 @@
 
 var React = require('react');
 
+var TooltipMixin = require('../tooltip/TooltipMixin');
+
 var DownloadProgressBar = React.createClass({
 
+	mixins: [
+		TooltipMixin
+	],
+
+	getDefaultProps: function () {
+		return {
+			tooltipOpenOn: 'manual',
+			enableTooltip: true
+		};
+	},
+
+	getInitialState: function () {
+		return {
+			tooltipTargetX: 0,
+			progress: 75
+		}
+	},
+
+	getTooltipElement: function () {
+		return this.refs.tooltipTarget.getDOMNode();
+	},
+
+	handleOnMouseMove: function (event) {
+		this.setState({
+			tooltipTargetX: event.pageX
+		});
+	},
+
 	render: function () {
+		var tooltipTargetStyles = {
+			left: this.state.tooltipTargetX
+		};
+
 		return (
 			<div className='download-progress-bar'>
-				<progress max='100' value='75'></progress>
+				<progress max='100' value={this.state.progress} onMouseMove={this.handleOnMouseMove}></progress>
+				<div ref='tooltipTarget' className='download-tooltip-target' style={tooltipTargetStyles}></div>
 			</div>
 		)
 	}
