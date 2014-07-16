fb33bbc08bfbdb7d35413ad645e447fb4fd1aa68
Jörn Röder <kontakt@joernroeder.de>
2014-07-16 18:02:59 +0200

    connection failed page style update

--- src/client/core/page/ConnectionFailedPage.js
+++ src/client/core/page/ConnectionFailedPage.js
@@ -17,16 +17,20 @@ var ConnectionFailedPage = React.createClass({
 		return (
 			<main className='main'>
 				<section className='page connection-failed'>
-					<header>
-						<h1>Connection Failed</h1>
-					</header>
-					<p>
-						Vestibulum id ligula porta felis euismod semper. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.
-					</p>
+					<div className='wrapper'>
+						<article>
+							<header>
+								<h1>Connection Failed</h1>
+							</header>
+							<p>
+								Vestibulum id ligula porta felis euismod semper. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.
+							</p>
 
-					<a href='#' className='btn' onClick={this.onReconnectClick}>
-						Reconnect
-					</a>
+							<a href='#' className='btn' onClick={this.onReconnectClick}>
+								Reconnect
+							</a>
+						</article>
+					</div>
 				</section>
 			</main>
 		)
