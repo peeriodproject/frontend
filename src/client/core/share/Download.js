cb851ffeba8ee26ddd9ccbce888e39e3754e0fd7
Jörn Röder <kontakt@joernroeder.de>
2014-07-23 14:43:23 +0200

    added moment and countdown js

--- src/client/core/share/Download.js
+++ src/client/core/share/Download.js
@@ -5,7 +5,11 @@
 
 var React = require('react');
 
+var countdown = require('countdown');
+
 var Download = React.createClass({
+
+	_updateTimeout: null,
 
 	getDefaultProps: function () {
 		return {
@@ -21,18 +25,40 @@ var Download = React.createClass({
 
 	getInitialState: function () {
 		return {
-			averageSpeed: 0
+			averageSpeed: 0,
+			now: 0,
+			timeLeft: 0
 		};
 	},
 
 	componentDidMount: function () {
 		this.calcTimeAndSpeed();
+
+		this._startUpdateTimeout();
 	},
 	// @see http://stackoverflow.com/a/7312237
 	componentWillReceiveProps: function (nextProps) {
 		this.calcTimeAndSpeed(nextProps);
 	},
 
+	_startUpdateTimeout: function () {
+		var _this = this;
+
+		if (this._updateTimeout) {
+			clearTimeout(this._updateTimeout);
+		}
+
+		this._updateTimeout = setTimeout(function () {
+			_this.calcTimeAndSpeed();
+			_this._startUpdateTimeout();
+		}, 200);
+	},
+
+	/**
+	 * Returns the progress as a decimal value 0 <= i <= 1
+	 *
+	 * @returns {number} the state of the download progress
+	 */
 	getProgress: function () {
 		var prog = (this.props.loaded / this.props.size);
 
@@ -54,12 +80,14 @@ var Download = React.createClass({
 	calcTimeAndSpeed: function (props) {
 		props = props || this.props;
 
-		var timeElapsed = new Date().getTime() - this.props.created;
+		var now = new Date().getTime();
+		var timeElapsed = now - this.props.created;
 		var speed = this.props.loaded / timeElapsed;
 		var timeLeft = (this.props.size - this.props.loaded) / speed;
 
 		this.setState({
 			timeLeft: timeLeft,
+			now: now,
 			averageSpeed: this.getAverageSpeed(speed * 1000) // ms to s
 		});
 	},
@@ -70,6 +98,10 @@ var Download = React.createClass({
 		
 		// exponential moving average
 		return SMOOTHING_FACTOR * lastSpeed + (1 - SMOOTHING_FACTOR) * this.state.averageSpeed;
+	},
+
+	getTimeLeft: function () {
+		return countdown(new Date().getTime() + this.state.timeLeft).toString();
 	},
 
 	render: function () {
@@ -89,7 +121,7 @@ var Download = React.createClass({
 					{this.getSizeWithExtension(this.props.loaded)} of {this.getSizeWithExtension(this.props.size)}
 				</span>
 				<span className='speed'>{this.getSizeWithExtension(this.state.averageSpeed)}/s</span>
-				<span className='time'>10 seconds remaining</span>
+				<span className='time'>{this.getTimeLeft()}</span>
 			</div>
 		)
 	}
